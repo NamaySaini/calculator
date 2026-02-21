3460c2dbd8a220027e79b8ca932c954aa87c6c47
Namay Saini <namaysaini@wireless-10-148-94-113.public.utexas.edu>
2026-02-20 23:12:50 -0600

    Add multiply function

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function subtract() {
 
  document.getElementById('result').textContent = 'Result: ' + result;
 }
+
+function multiply() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+
+ const num2 = parseFloat(document.getElementById('num2').value);
+
+ const result = num1 * num2;
+
+ document.getElementById('result').textContent = 'Result: ' + result;
+}
